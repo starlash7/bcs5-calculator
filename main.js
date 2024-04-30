a6206811e1bc6884ad5bad365165d3423437b8ff
starlash7 <yang92sw@gmail.com>
2024-04-30 13:49:42 +0900

    onClickNumber

--- main.js
+++ main.js
@@ -3,3 +3,15 @@ let result = 0;
 const resultEl = document.querySelector(".result");
 
 resultEl.innerHTML = result;
+
+// 13글자가 넘어가지면 입력이 안되게
+
+function onClickNumber(number) {
+  if (resultEl.innerHTML.length >= 13) return;
+
+  if (resultEl.innerHTML === "0") {
+    resultEl.innerHTML = number;
+  } else {
+    resultEl.innerHTML += number;
+  }
+}
